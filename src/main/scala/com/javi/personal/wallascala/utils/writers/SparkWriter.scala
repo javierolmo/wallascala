34a5b19c46a8a3b29021c70c658fe30c20aa8dcb
Javier Olmo Injerto <olmo.injerto.javier@gmail.com>
2023-12-02 14:08:33 +0100

    Writers refactoring

--- src/main/scala/com/javi/personal/wallascala/utils/writers/SparkWriter.scala
+++ src/main/scala/com/javi/personal/wallascala/utils/writers/SparkWriter.scala
@@ -2,8 +2,20 @@ package com.javi.personal.wallascala.utils.writers
 
 import org.apache.spark.sql.{DataFrame, DataFrameWriter, Row, SparkSession}
 
+/**
+ * SparkWriter is an abstract class that defines the basic methods to write a DataFrame.
+ * @param format The format of the DataFrame to write.
+ * @param saveMode The save mode of the DataFrame to write.
+ * @param options The options of the DataFrame to write.
+ * @param spark The SparkSession to use.
+ */
 abstract class SparkWriter(format: String, saveMode: String, options: Map[String, String])(implicit spark: SparkSession) {
 
+  /**
+   * This method returns a DataFrameWriter[Row] with the basic configuration.
+   * @param dataFrame The DataFrame to write.
+   * @return A DataFrameWriter[Row] with the basic configuration.
+   */
   protected def baseWriter(dataFrame: DataFrame): DataFrameWriter[Row] =
     dataFrame.write
       .mode(saveMode)
@@ -11,6 +23,11 @@ abstract class SparkWriter(format: String, saveMode: String, options: Map[String
       .options(options)
 
 
+  /**
+   * This method writes a DataFrame. It is an abstract method that must be implemented by the subclasses.
+   * @param dataFrame The DataFrame to write.
+   * @param spark The SparkSession to use.
+   */
   def write(dataFrame: DataFrame)(implicit spark: SparkSession): Unit
 
 }
